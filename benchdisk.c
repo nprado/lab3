0e1bc748ab8028c9f98db92976bfae399bea0696
Daniel <daniel.prado@gmail.com>
2011-04-11 15:04:38 -0300

    versao nova : entrada como parametro

--- benchdisk.c
+++ benchdisk.c
@@ -17,45 +17,38 @@ int main (int argc, char *argv[]){
   long double somatempo;
   long lSize;
   int i, j;
-  char name_file[25];
   
-    init_time = clock();
-  for (j= 0; j < 2 ; j++){
-
-    for (i = 1; i <102; i++){
-      
-      if (j ==0) sprintf(name_file,"C3.gap_f2b.D.%d.bin.gz", i);
-      if (j ==1) sprintf(name_file, "T1.gap_f2b.I.%d.bin.gz",i);
-      
-      printf("%s\n", name_file);
-      file = fopen(name_file  , "r");
-      if (file == NULL ) {  fprintf( stderr,"File error"); return 2; }
-      
-      fseek (file , 0 , SEEK_END);
-      lSize = ftell (file);
-      rewind (file);
-      
-      // alocate memory to contain hole file 
+ 
     
-      buffer = (char *)malloc (sizeof (char )*lSize);
-    
-      if (buffer == NULL) {fprintf ( stderr,"Memory error "); return 3;}
-      //counting time of read
-    
-
-      result = fread(buffer, 4000, (lSize / 4000 ), file);
-   
-      //if(result != lSize) {fprintf( stderr,"Read Error"); return 4; }
       
-   
-   
+  printf("%s\n", argv[1]  );
+  file = fopen(argv[1]    , "r");
+  if (file == NULL ) {  fprintf( stderr,"File error"); return 1; }
+  
+  fseek (file , 0 , SEEK_END);
+  lSize = ftell (file);
+  rewind (file);
+      
+  // alocate memory to contain hole file 
+    
+  buffer = (char *)malloc (sizeof (char )*lSize);
+    
+  if (buffer == NULL) {fprintf ( stderr,"Memory error "); return 2;}
+  //counting time of read
+  init_time = clock();
+  
+  result = fread(buffer, 4000, (lSize / 4000 ), file);
+  
+  //if(result != lSize) {fprintf( stderr,"Read Error"); return 4; }
+      
+  end_time = clock(); 
+  
 
-      fclose(file);
-      free(buffer);
+  fclose(file);
+  free(buffer);
       //somatempo += t ;
-    }
-  }
-  end_time = clock();
+  
+  
   
   t = ((long double )(end_time - init_time) / (long double )CLOCKS_PER_SEC ) ;
   printf("fread takes %.40Lf s\n", t);
